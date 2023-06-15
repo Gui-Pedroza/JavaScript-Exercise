0dfab8cdadeb42b152d997fd8b9fea123cebcdd7
Guilherme Pedroza <109224884+Gui-Pedroza@users.noreply.github.com>
2023-06-15 07:37:35 -0300

    testing loops

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 function contar() {
-    var inicio = document.getElementById('inicio')
-    var fim = document.getElementById('fim')
-    var passo = document.getElementById('passo')
+    var inicio = document.getElementById('inicio').value
+    var fim = document.getElementById('fim').value
+    var passo = document.getElementById('passo').value
     var res = document.getElementById('resultado')
 
-    
+    if (inicio == "" || fim == "" || passo == "") {
+        res.innerHTML = 'Impossível contar!'
+        inicio.focus
+    } else {
+        if (passo == 0) {
+            window.alert('Passo não pode ser igual a 0, considerando PASSO = 1')
+            passo = 1
+        }
+        for (var i = Number(inicio); i < Number(fim); Number(passo)++) {
+            var contando = document.createElement('p')
+            contando.setAttribute('id', 'paragrafo')
+            contando.innerHTML = 'Contando'
+            res.innerHTML += inicio + passo
+        }
+    }
+
 }
